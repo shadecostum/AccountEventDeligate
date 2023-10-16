cbcef7cf800d34b1db2e1d1b18f5eeb8ef65e287
Akash santhosh <akash777kk@gmail.com>
2023-10-16 12:53:22 +0530

    Add project files.

---
+++ AccountEventDeligate/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountEventDeligate.Models
+{
+    //delegate void DBalanceChange(Account account);
+    internal class Account
+    {
+        public int AccountId { get; set; }
+        public string AccountName { get; set; }
+        public double AccountBalance { get; set; }
+
+        public event Action<Account> AccountBalanceChange;
+
+        public void DepositeAccount(double accountBalance)
+        {
+            AccountBalance += accountBalance;
+            if (AccountBalanceChange != null)
+                AccountBalanceChange(this);
+        }
+
+    }
+}
---
+++ AccountEventDeligate/Program.cs
@@ -0,0 +1,30 @@
+﻿using AccountEventDeligate.Models;
+using System.Security.Principal;
+
+namespace AccountEventDeligate
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Account account1 = new Account() {AccountId=1,AccountName="Akash",AccountBalance=500,};
+            account1.AccountBalanceChange += SendSms;
+            account1.AccountBalanceChange += SendEmail;
+            account1.AccountBalanceChange += (a) =>
+            {
+                Console.WriteLine($"sending Whatsapp{a.AccountName} of deposited{a.AccountBalance}");
+            };
+            account1.DepositeAccount(200);
+            account1.DepositeAccount(100);
+        }
+        static void SendSms(Account account)
+        {
+            Console.WriteLine($"sending sms{account.AccountName} of deposited{account.AccountBalance}");
+        }
+        static void SendEmail(Account account)
+        {
+            Console.WriteLine($"sending Email{account.AccountName} of deposited{account.AccountBalance}");
+        }
+    }
+}
